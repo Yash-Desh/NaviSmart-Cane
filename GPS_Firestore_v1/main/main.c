b0412cbb56f84a71cd55b86384079ff10ac4b31b
Yash Deshpande <23yashd@gmail.com>
2024-05-08 01:12:58 +0530

    GPS+Firestore v1 code added

--- GPS_Firestore_v1/main/main.c
+++ GPS_Firestore_v1/main/main.c
@@ -1,12 +1,26 @@
+// Author : Yash Deshpande
+// Date : 08-05-2024
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/param.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
+#include "freertos/timers.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_http_client.h"
+#include "esp_http_server.h"  // Not being used
+#include "my_data.h"
+#include "esp_system.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
+
+
 
 static const int RX_BUF_SIZE = 2024;
 
@@ -39,6 +53,11 @@ void init(void)
     uart_set_pin(UART_NUM_0, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
+
+double lat, longi;
+char latitude[100] = "empty";
+char longitude[100] = "empty";
+
 // for UART2
 //  For communication between GPS & esp32
 void init2(void)
@@ -68,9 +87,8 @@ static void rx_task(void *arg)
 
     int comma_count = 0;
 
-    double lat, longi;
-    char latitude[100] = "empty";
-    char longitude[100] = "empty";
+    
+    
     
     char *lat_ptr;
     char *longi_ptr;
@@ -148,9 +166,128 @@ static void rx_task(void *arg)
     free(str2);
 }
 
+
+
+
+
+extern const uint8_t certificate_pem_start[] asm("_binary_certificate_pem_start");
+extern const uint8_t certificate_pem_end[] asm("_binary_certificate_pem_end");
+
+
+// WiFi
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        printf("WiFi connecting ... \n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi connected ... \n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection ... \n");
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        printf("WiFi got IP ... \n\n");
+        break;
+    default:
+        break;
+    }
+}
+
+void wifi_connection()
+{
+    // 1 - Wi-Fi/LwIP Init Phase
+    esp_netif_init();                    // TCP/IP initiation 					s1.1
+    esp_event_loop_create_default();     // event loop 			                s1.2
+    esp_netif_create_default_wifi_sta(); // WiFi station 	                    s1.3
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&wifi_initiation); // 					                    s1.4
+    
+    // 2 - Wi-Fi Configuration Phase
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+    wifi_config_t wifi_configuration = {
+        .sta = {
+            .ssid = SSID,
+            .password = PASS}};
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    
+    // 3 - Wi-Fi Start Phase
+    esp_wifi_start();
+    
+    // 4- Wi-Fi Connect Phase
+    esp_wifi_connect();
+
+}
+
+
+// Client
+esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt)
+{
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ON_DATA:
+        printf("Client HTTP_EVENT_ON_DATA: %.*s\n", evt->data_len, (char *)evt->data);
+        break;
+
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+void post_rest_function()
+{
+    esp_http_client_config_t config_get = {
+        .url = "https://firestore.googleapis.com/v1/projects/smart-navi-stick/databases/(default)/documents/test",
+        .method = HTTP_METHOD_POST,
+        .cert_pem = (const char *)certificate_pem_start,
+        .event_handler = client_event_get_handler};
+    
+    esp_http_client_handle_t client = esp_http_client_init(&config_get);
+
+    char json_string[1024];
+    snprintf(json_string, 1024,
+         "{\"fields\":{\"Memory\":{\"doubleValue\":%f},\"Name\":{\"doubleValue\":\"%f\"}}}",
+         lat, longi);
+
+    // char s1[] = "{\"fields\":{\"latitude\":{\"stringValue\":\"";
+    // char s2[] = "\"},\"longitude\":{\"stringValue\":\"";
+    // char s3[] = "\"}}}";
+    // strcat(s1,latitude);
+    // strcat(s1,s2);
+    // strcat(s1, longitude);
+    // strcat(s1, s3);
+
+    char *post_data = json_string;
+    // char *post_data = "{\"fields\":{\"Memory\":{\"doubleValue\":\"2\"},\"Name\":{\"stringValue\":\"Old ESP32\"}}}";
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    
+    
+    esp_http_client_perform(client);
+    esp_http_client_cleanup(client);
+}
+
+
 void app_main(void)
 {
     init();
     init2();
     xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES, NULL);
+
+    // Non-volatile Storage
+    nvs_flash_init();
+
+    // 
+    wifi_connection();
+
+    // REST GET data
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    printf("WIFI was initiated.................\n\n");
+    
+    post_rest_function();
+
 }
